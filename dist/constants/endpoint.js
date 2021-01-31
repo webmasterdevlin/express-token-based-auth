6205f4c4ad6a72678f325d8ed80f3e98860f4572
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-01-31 20:42:49 +0100

    🎨

--- dist/constants/endpoint.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SERVER_STATUS_ENDPOINT = exports.BASE_ENDPOINT = void 0;
-// Define ENDPOINT 
-exports.BASE_ENDPOINT = "/api";
-exports.SERVER_STATUS_ENDPOINT = exports.BASE_ENDPOINT + "/server-status";
